c69fc35eaafa0b8b1f6ae58e2cfd132f174bb5d4
anthousecode <bsmgroup69@gmail.com>
2018-08-22 23:40:46 +0300

    footer markup, resposive design and slides text

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 // ------- SLIDER
 
-let sliderImagesSrc = ['images/slider-img.jpg', 'images/post-1-img.jpg'],
+let slides = [
+  {src:'images/slider-img.jpg', date:'20 August 2017', text:'Kuril Islands'},
+  {src:'images/post-1-img.jpg', date:'20 August 2017', text:'Kuril Islands'}
+],
   curIndex = -1;
 
 function slideShow() {
   document.getElementById('slider').className += 'fadeOut';
   setTimeout(() => {
-    document.getElementById('sliderImg').src = sliderImagesSrc[curIndex];
+    document.getElementById('sliderImg').src = slides[curIndex].src;
+    document.getElementById('slideDate').innerHTML = slides[curIndex].date;
+    document.getElementById('slideTitle').innerHTML = slides[curIndex].text;
     document.getElementById('slider').className = '';
   }, 500);
   curIndex++;
-  if (curIndex == sliderImagesSrc.length) {
+  if (curIndex == slides.length) {
     curIndex = 0;
   }
   setTimeout(slideShow, 5000);
