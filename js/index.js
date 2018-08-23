bef4ad7623702c264b9c15bb0f32780ac1a0d6e0
NatashaElElistratova <natalia.yelistratova@gmail.com>
2018-08-23 15:43:10 +0300

    add modal window with form and change bootstrap connection way

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 // ------- SLIDER
 
 let slides = [
-  {src:'images/slider-img.jpg', date:'20 August 2017', text:'Kuril Islands'},
-  {src:'images/post-1-img.jpg', date:'20 August 2017', text:'Kuril Islands'}
-],
+    {
+      src: 'images/slider-img.jpg',
+      date: '20 August 2017',
+      text: 'Kuril Islands'
+    },
+    {
+      src: 'images/post-1-img.jpg',
+      date: '20 August 2017',
+      text: 'Kuril Islands'
+    }
+  ],
   curIndex = -1;
 
 function slideShow() {
@@ -21,3 +29,38 @@ function slideShow() {
   setTimeout(slideShow, 5000);
 }
 slideShow();
+
+//----------- CONTACT FORM
+let link = document.getElementById('contactLink');
+let modal = document.getElementById('contactForm');
+let btn = document.getElementById('btnClose');
+
+btn.addEventListener('click', onFormHide);
+link.addEventListener('click', onFormShow);
+
+function onFormShow() {
+  modal.style.display = 'flex';
+  fadeIn(modal);
+}
+
+function onFormHide() {
+  modal.style.opacity = '0';
+  modal.style.display = 'none';
+}
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+
+  var last = +new Date();
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / 400;
+    last = +new Date();
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+        setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
